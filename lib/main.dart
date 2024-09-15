084a582c1f9f87e1cb89088cd6c35a907344b7d1
AVRSANAND <avrsanand2003@gmail.com>
2024-09-15 14:53:29 +0530

    Title Bar

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,19 @@ import 'package:first_app/gradient_container.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
+      title: 'Dice Roller',
       home: Scaffold(
-        body: GradientContainer(
-          colorsGradient:[
+        appBar: AppBar(
+          title: const Text('Dice Roller'),
+          titleTextStyle: const TextStyle(
+            fontFamily: 'Times New Roman',
+            fontSize: 24,
+            color: Color.fromARGB(255, 117, 62, 62),
+          ),
+        ),
+        body: const GradientContainer(
+          colorsGradient: [
             Color.fromARGB(255, 147, 86, 253),
             Color.fromARGB(255, 249, 131, 95),
           ],
